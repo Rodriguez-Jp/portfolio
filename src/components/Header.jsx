d15c5cba0d99da9291feddd519055c849bce515a
Juan Pablo Rodriguez Becerra <juanrdrz16@gmail.com>
2023-09-14 22:30:14 -0500

    Added anchor for header elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,21 @@ export default function Header() {
   return (
     <header className="flex justify-between text-white font-game p-4 z-20">
       <div className="flex justify-center items-center">
-        <p className="text-xl">Juan Rodriguez</p>
+        <p className="text-xl">
+          <a href="#">Juan Rodriguez</a>
+        </p>
       </div>
       <nav className="md:text-lg">
         <ul className="flex">
-          <li className="p-2">Home</li>
-          <li className="p-2 hidden md:block">Projects</li>
-          <li className="p-2">Contact me</li>
+          <li className="p-2">
+            <a href="#">Home</a>
+          </li>
+          <li className="p-2 hidden md:block">
+            <a href="#projects">Projects</a>
+          </li>
+          <li className="p-2">
+            <a href="#contact-me">Contact me</a>
+          </li>
         </ul>
       </nav>
     </header>
